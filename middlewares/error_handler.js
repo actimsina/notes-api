910c1d79071bc0274a4fef33c288e8f8f461f6fe
Achyut <actimsina@gmail.com>
2023-07-13 08:37:09 +0545

    error-handlers improved

--- middlewares/error_handler.js
+++ middlewares/error_handler.js
@@ -1,8 +1,18 @@
 const errorHandler = (err, req, res, next) => {
-    console.log(err.stack)
+    console.error(err)
     res.statusCode = res.statusCode ? res.statusCode : 500
     if (err.name === 'ValidationError') res.statusCode = 400
     res.json({ error: err.message })
 }
 
-module.exports = errorHandler 
+const errorHandler1 = (err, req, res, next) => {
+    let statusCode = res.statusCode || 500
+    let errMessage = err.message || 'Internal Server Error'
+
+    console.error(err)
+    res.status(statusCode).json({ error: errMessage })
+}
+
+
+
+module.exports = errorHandler1;
